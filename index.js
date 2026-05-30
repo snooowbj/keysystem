3807f6f45ae4d5c1594b98db8847dced377dac1a
snooowbj <twistoficialqi0@gmail.com>
2026-05-29 21:45:35 -0300

    debug token

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+console.log("TOKEN =", process.env.TOKEN);
 const express = require("express");
 const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const { v4: uuidv4 } = require("uuid");
